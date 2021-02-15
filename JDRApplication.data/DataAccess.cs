6e7615d5c00529fcd207036af803c46b2b3760e4
Valérian 9 <valerian.gibault@gmail.com>
2021-02-15 16:48:02 +0100

    view + entity

--- JDRApplication.data/DataAccess.cs
+++ JDRApplication.data/DataAccess.cs
@@ -1,4 +1,6 @@
-﻿using RestSharp;
+﻿using JDRApplication.data.Entities;
+using Newtonsoft.Json;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -14,10 +16,52 @@ namespace JDRApplication.data
         private static string apiKey = "ba77b40bf106652ec24b73f0c92e4fbb9f66f";
 
 
-        public static void GetAll()
+        public static List<CharacterEntity> GetAll()
         {
             RestClient client = new RestClient(connectionString);
             var request = new RestRequest(Method.GET);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("x-apikey", "ba77b40bf106652ec24b73f0c92e4fbb9f66f");
+            request.AddHeader("content-type", "application/json");
+            IRestResponse response = client.Execute(request);
+            List<CharacterEntity> ret = new List<CharacterEntity>(); 
+            try
+            {
+                ret = JsonConvert.DeserializeObject<List<CharacterEntity>>(response.Content);
+            }
+            catch(Exception e)
+            {
+
+            }
+            return ret;
+        }
+
+        public static void Insert()
+        {
+            var client = new RestClient("https://jdrcharacters-f0a4.restdb.io/rest/characters");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("x-apikey", "ba77b40bf106652ec24b73f0c92e4fbb9f66f");
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("application/json", "{\"field1\":\"xyz\",\"field2\":\"abc\"}", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+        }
+
+        public static void Update()
+        {
+            var client = new RestClient("https://jdrcharacters-f0a4.restdb.io/rest/characters/(ObjectID)");
+            var request = new RestRequest(Method.PUT);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("x-apikey", "ba77b40bf106652ec24b73f0c92e4fbb9f66f");
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("application/json", "{\"field2\":\"new value\"}", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+        }
+
+        public static void Delete()
+        {
+            var client = new RestClient("https://jdrcharacters-f0a4.restdb.io/rest/characters/(ObjectID)");
+            var request = new RestRequest(Method.DELETE);
             request.AddHeader("cache-control", "no-cache");
             request.AddHeader("x-apikey", "ba77b40bf106652ec24b73f0c92e4fbb9f66f");
             request.AddHeader("content-type", "application/json");
---
+++ JDRApplication.data/Entities/CharacterEntity.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JDRApplication.data.Entities
+{
+    public class CharacterEntity : CharacterData
+    {
+        public CharacterType EnumType { get; set; }
+    }
+    
+
+    public class CharacterData
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Race { get; set; }
+        public string Extraction { get; set; }
+        public int Physique { get; set; }
+        public int Force { get; set; }
+        public int Dexterite { get; set; }
+        public int Mental { get; set; }
+        public int Savoir { get; set; }
+        public int Instinct { get; set; }
+        public int Social { get; set; }
+        public int Relation { get; set; }
+        public int Aura { get; set; }
+        public string Description { get; set; }
+    }
+
+    public enum CharacterType
+    {
+        ForcePure, // Guerrier barabar
+        DexPure, // Archer lancier
+        SavoirPure, // Mage citadin
+        InstinctPure, // Shaman
+        RelationPure, // compte electeur manipulateur Orateur
+        AuraPure, // Général Capitaine Agitateur
+        DexteriteInstinct, // Chasseur vagabond archer grand pied
+        ForceInstinct, // nomade, héraut élémentaire 
+        ForceSavoir, // Mage de bataille 
+        SavoirRelation, // Mage de la cour
+        AuraInstinct, // Mage de soutiens 
+        ForceAura, // Champion
+        DexteriteRelation // voleur
+    }
+}
